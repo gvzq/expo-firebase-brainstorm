c265bc83b76abcd1c5e09af7433bae0260d8cc8e
Gerardo Vazquez <gvzq@hotmail.com>
2022-12-23 21:52:32 -0600

    feat: firebase db and todo app
    Displays notes realtime.

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 import Constants from 'expo-constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -7,6 +8,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const firebaseConfig = {
   apiKey: Constants.manifest.extra.apiKey,
   authDomain: Constants.manifest.extra.authDomain,
+  databaseURL: Constants.manifest.extra.databaseURL,
   projectId: Constants.manifest.extra.projectId,
   storageBucket: Constants.manifest.extra.storageBucket,
   messagingSenderId: Constants.manifest.extra.messagingSenderId,
@@ -21,4 +23,6 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
-export { auth };
+const database = getDatabase(app);
+
+export { auth, database};
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,182 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
 import { signOut } from 'firebase/auth';
-
 import { auth } from '../config';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  ScrollView
+} from 'react-native';
+import {
+  Text,
+  Checkbox,
+  TextInput,
+  Button
+} from 'react-native-paper';
+import {
+  ref,
+  onValue,
+  push,
+  update,
+  remove,
+  getDatabase
+} from 'firebase/database';
 
 export const HomeScreen = () => {
+  const [todos, setTodos] = useState({});
+  const [presentTodo, setPresentTodo] = useState('');
+  const todosKeys = Object.keys(todos);
+  const database = getDatabase();
+
   const handleLogout = () => {
     signOut(auth).catch(error => console.log('Error logging out: ', error));
   };
+
+  useEffect(() => {
+    return onValue(ref(database, '/todos'), querySnapShot => {
+      let data = querySnapShot.val() || {};
+      let todoItems = { ...data };
+      setTodos(todoItems);
+    });
+
+  }, []);
+
+  function addNewTodo() {
+    push(ref(database, '/todos'), {
+      done: false,
+      title: presentTodo,
+    });
+    setPresentTodo('');
+  }
+
+  function clearTodos() {
+    remove(ref(database, '/todos'));
+  }
+
+  const user = auth.currentUser;
+  if (user !== null) {
+    console.log(user.providerData[0].email);
+  }
+
   return (
-    <View style={styles.container}>
-      <Button title='Sign Out' onPress={handleLogout} />
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainerStyle}>
+      <View style={styles.container}>
+        <Button mode="outlined" onPress={handleLogout} >
+          Sign Out
+        </Button>
+      </View>
+      <View>
+        {todosKeys.length > 0 ? (
+          todosKeys.map(key => (
+            <ToDoItem
+              key={key}
+              id={key}
+              todoItem={todos[key]}
+            />
+          ))
+        ) : (
+          <Text>No todo item</Text>
+        )}
+      </View>
+
+      <TextInput
+        placeholder="New todo"
+        value={presentTodo}
+        style={styles.textInput}
+        onChangeText={text => {
+          setPresentTodo(text);
+        }}
+        onSubmitEditing={addNewTodo}
+      />
+
+      <View>
+        <View style={{ marginTop: 20 }}>
+          <Button mode="outlined"
+            onPress={addNewTodo}
+            disabled={presentTodo == ''}>
+            Add new todo
+          </Button>
+        </View>
+
+        <View style={{ marginTop: 20 }}>
+          <Button mode="outlined" onPress={clearTodos}>
+            Clear the todo list
+          </Button>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const ToDoItem = ({ todoItem: { title, done }, id }) => {
+  const [doneState, setDone] = useState(done);
+  const database = getDatabase();
+
+  const onCheck = () => {
+    console.log(doneState);
+    setDone(!doneState);
+    update(ref(database, '/todos'), {
+      [id]: {
+        title,
+        done: !doneState,
+      },
+    });
+  };
+  return (
+    <View style={styles.todoItem}>
+      <Checkbox
+        onPress={onCheck}
+        status={doneState ? 'checked' : 'unchecked'}
+      // value={doneState}
+      />
+      <Text style={[styles.todoText, { opacity: doneState ? 0.2 : 1 }]}>
+        {title}
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+    paddingTop: 12
+  },
+  contentContainerStyle: {
+    padding: 24
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#afafaf',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    fontSize: 20,
+  },
+  todoItem: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+  todoText: {
+    paddingHorizontal: 5,
+    fontSize: 16
+  },
 });
+
+// export const HomeScreen = () => {
+//   const handleLogout = () => {
+//     signOut(auth).catch(error => console.log('Error logging out: ', error));
+//   };
+//   return (
+//     <View style={styles.container}>
+//       <Button title='Sign Out' onPress={handleLogout} />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//   }
+// });
